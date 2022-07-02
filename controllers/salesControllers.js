04e493d2cf2c73e9b9642b74dc2d3b35b305ca74
Leonidas <leonidas.drawanz@gmail.com>
2022-07-02 13:44:34 -0300

    adicionando salesControllers

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -1,2 +1,29 @@
-// const salesServices = require("../services/salesServices");
-// const httpStatus = require("../helpers/httpStatusCode");
+const salesServices = require('../services/salesServices');
+const httpStatus = require('../helpers/httpStatusCode');
+
+const registrySales = async (req, res) => {
+  try {
+    const validateReqBody = await salesServices.validateBodyReq(req.body);
+    const validateProductId = await salesServices.validadeProductId(req.body);
+
+    if (validateReqBody.message) {
+      return res
+        .status(validateReqBody.status)
+        .json(validateReqBody.message);
+    }
+    if (validateProductId.message) {
+      return res
+        .status(validateReqBody.status)
+        .json(validateReqBody.message);
+    }
+
+    const response = await salesServices.registerSales();
+    return res.status(httpStatus.CREATED).json(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  registrySales,
+};
