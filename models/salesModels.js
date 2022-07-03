506ba37368e21dcb769c26efafa5938bf44e3761
Leonidas <leonidas.drawanz@gmail.com>
2022-07-03 11:20:43 -0300

    fix: ajustando a query para filtragem de sales por id

--- models/salesModels.js
+++ models/salesModels.js
@@ -1,9 +1,10 @@
 const connection = require('../helpers/connection');
 
 const query1 = 'SELECT SP.sale_id AS saleId, S.date, SP.product_id AS productId, SP.quantity';
+const query1findById = 'SELECT S.date, SP.product_id AS productId, SP.quantity';
 const query2 = ' FROM StoreManager.sales';
 const query3 = ' AS S INNER JOIN StoreManager.sales_products AS SP ON S.id = SP.sale_id';
-const query4 = ' WHERE SP.product_id = ?';
+const query4 = ' WHERE SP.sale_id = ?';
 const query5 = ' ORDER BY sale_id ASC , product_id ASC';
 
 const registerSales = async (sales) => {
@@ -22,7 +23,10 @@ const registerSales = async (sales) => {
 };
 
 const findSaleById = async (id) => {
-  const [sales] = await connection.execute(query1 + query2 + query3 + query4 + query5, [id]);
+  const [sales] = await connection.execute(
+    query1findById + query2 + query3 + query4 + query5,
+    [id],
+  );
   return sales;
 };
 
