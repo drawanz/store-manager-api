e8818083fcf335c813e9b3cfee9b674b3d80ec21
Leonidas <leonidas.drawanz@gmail.com>
2022-07-03 10:29:41 -0300

    feat: criando as rotas para encontrar todas as vendas e as vendas por ID

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ const router = express.Router();
 router.get('/products', productsControllers.getAll);
 router.post('/products', productsControllers.add);
 router.get('/products/:id', productsControllers.getById);
+
+router.get('/sales', salesController.findAllSales);
+router.get('/sales/:id', salesController.findSaleById);
 router.post('/sales', salesController.registrySales);
 
 module.exports = router;
