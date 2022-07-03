bd7181ea5969af5cba09142068e801abb1483da2
Leonidas <leonidas.drawanz@gmail.com>
2022-07-03 11:16:51 -0300

    fix: ajustando as querys na camada de models para obter o objeto com as propriedades no formato correto

--- models/salesModels.js
+++ models/salesModels.js
@@ -1,9 +1,10 @@
 const connection = require('../helpers/connection');
 
-const query1 = 'SELECT SP.sale_id, S.date, SP.product_id, SP.quantity FROM StoreManager.sales';
-const query2 = ' AS S INNER JOIN StoreManager.sales_products AS SP ON S.id = SP.sale_id';
-const query3 = ' WHERE SP.product_id = ?';
-const query4 = ' ORDER BY sale_id ASC , product_id ASC';
+const query1 = 'SELECT SP.sale_id AS saleId, S.date, SP.product_id AS productId, SP.quantity';
+const query2 = ' FROM StoreManager.sales';
+const query3 = ' AS S INNER JOIN StoreManager.sales_products AS SP ON S.id = SP.sale_id';
+const query4 = ' WHERE SP.product_id = ?';
+const query5 = ' ORDER BY sale_id ASC , product_id ASC';
 
 const registerSales = async (sales) => {
   await connection.execute('INSERT INTO StoreManager.sales (`date`) VALUES (NOW())');
@@ -21,12 +22,12 @@ const registerSales = async (sales) => {
 };
 
 const findSaleById = async (id) => {
-  const [sales] = await connection.execute(query1 + query2 + query3 + query4, [id]);
+  const [sales] = await connection.execute(query1 + query2 + query3 + query4 + query5, [id]);
   return sales;
 };
 
 const findAllSales = async () => {
-  const [sales] = await connection.execute(query1 + query2 + query4);
+  const [sales] = await connection.execute(query1 + query2 + query3 + query5);
   return sales;
 };
 
