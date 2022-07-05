f74f322bfb109b44169cd2fd702fe18405d399bf
Leonidas <leonidas.drawanz@gmail.com>
2022-07-05 16:54:17 -0300

    fix: corrigindo erros de lint nos testes da productsControllers

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -4,86 +4,77 @@ const { expect } = require('chai');
 const productsController = require('../../../controllers/productsControllers');
 const productsService = require('../../../services/productsServices');
 
-describe("Desenvolve os testes para a camada de product da Controllers", async () => {
-  describe("verifica a função getAll com caso de sucesso", () => {
+describe('Desenvolve os testes para a camada de product da Controllers', () => {
+  describe('Verifica a função getAll com caso de sucesso', () => {
     const res = {};
     const req = {};
     const payloadOk = [
       {
         id: 1,
-        name: "Shampoo",
+        name: 'Shampoo',
       },
     ];
-
-    before(async () => {
+    before(() => {
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns(payloadOk);
-      sinon.stub(productsService, "getAll").resolves(payloadOk);
+      sinon.stub(productsService, 'getAll').resolves(payloadOk);
     });
-
-    after(async () => {
-      await productsService.getAll.restore();
+    after(() => {
+      productsService.getAll.restore();
     });
-
-    it("verifica se a função getAll retorna os dados corretos", async () => {
+    it('Testa se a função getAll retorna os dados corretos', async () => {
       await productsController.getAll(req, res);
       expect(res.json.calledWith(payloadOk)).to.equal(true);
       expect(res.status.calledWith(200)).to.be.equal(true);
     });
   });
 
-  describe("verifica a função getById com caso de sucesso", () => {
+  describe('Verifica a função getById com caso de sucesso', () => {
     const res = {};
     const req = {};
     const payloadId = 1;
     const payloadOk = [
       {
         id: 1,
-        name: "Shampoo",
+        name: 'Shampoo',
       },
     ];
-
-    before(async () => {
+    before(() => {
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns(payloadOk);
       req.params = sinon.stub().returns(payloadId);
-      sinon.stub(productsService, "getById").returns(payloadOk);
+      sinon.stub(productsService, 'getById').returns(payloadOk);
     });
-
-    after(async () => {
-      await productsService.getById.restore();
+    after(() => {
+      productsService.getById.restore();
     });
-
-    it("verifica se a função getById retorna os dados corretos", async () => {
+    it('Testa se a função getById retorna os dados corretos', async () => {
       await productsController.getById(req, res);
       expect(res.json.calledWith(payloadOk)).to.equal(true);
       expect(res.status.calledWith(200)).to.be.equal(true);
     });
   });
 
-  describe("verifica a função add com caso de sucesso", () => {
+  describe('Verifica a função add com caso de sucesso', () => {
     const res = {};
     const req = {};
     const payloadId = 1;
     const payloadOk = [
       {
         id: 1,
-        name: "Shampoo",
+        name: 'Shampoo',
       },
     ];
-
-    before(async () => {
+    before(() => {
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns(payloadOk);
       req.body = sinon.stub().returns(payloadId);
-      sinon.stub(productsService, "add").returns(payloadOk);
+      sinon.stub(productsService, 'add').returns(payloadOk);
     });
-
-    after(async () => {
-      await productsService.add.restore();
+    after(() => {
+      productsService.add.restore();
     });
-
-    it("verifica se a função add retorna os dados corretos", async () => {
+    it('Testa se a função add retorna os dados corretos', async () => {
       await productsController.add(req, res);
       expect(res.json.calledWith(payloadOk)).to.equal(true);
       expect(res.status.calledWith(201)).to.be.equal(true);
