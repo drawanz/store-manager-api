cd14a9a60140ef843d0050842ed6c8ae5099539a
Leonidas <leonidas.drawanz@gmail.com>
2022-07-06 11:32:48 -0300

    feat: adicionando a rota put para atualização de produtos

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/products', productsControllers.getAll);
 router.post('/products', productsControllers.add);
 router.get('/products/:id', productsControllers.getById);
+router.put('/products/:id', productsControllers.att);
 
 router.get('/sales', salesController.findAllSales);
 router.get('/sales/:id', salesController.findSaleById);
