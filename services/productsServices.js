30b951ee92f7252e896464d59a17dee8d461acdd
Leonidas <leonidas.drawanz@gmail.com>
2022-07-06 11:31:54 -0300

    feat: adicioando a funcionalidade de atualização de produto na camada services
    funções adicionadas: validadeProductId, validateName, att

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,8 +1,8 @@
-const managerModel = require('../models/productsModels');
+const productsModels = require('../models/productsModels');
 const httpStatus = require('../helpers/httpStatusCode');
 
 const getAll = async () => {
-  const response = await managerModel.getAll();
+  const response = await productsModels.getAll();
 
   if (!response) return [];
 
@@ -10,7 +10,7 @@ const getAll = async () => {
 };
 
 const getById = async (id) => {
-  const [response] = await managerModel.getById(id);
+  const [response] = await productsModels.getById(id);
 
   if (!response) return [];
 
@@ -32,15 +32,56 @@ const add = async (nameProduct) => {
     };
   }
 
-  const [response] = await managerModel.add(nameProduct);
+  const [response] = await productsModels.add(nameProduct);
 
   if (!response) return [];
   
   return response;
 };
 
+const validateProductId = async ({ id }) => {
+  const allProducts = await productsModels.getAll();
+  const validIds = allProducts.map((e) => e.id);
+  console.log();
+  if (!validIds.includes(Number(id))) {
+    return {
+      status: httpStatus.NOT_FOUND,
+      message: 'Product not found',
+    };
+  }
+  return 'validação ok';
+};
+
+const validateName = async ({ name }) => {
+  if (!name) {
+    return {
+      status: httpStatus.BAD_REQUEST,
+      message: '"name" is required',
+    };
+  }
+
+  if (name.length < 5) {
+    return {
+      status: httpStatus.UNPROCESSABLE_ENTITY,
+      message: '"name" length must be at least 5 characters long',
+    };
+  }
+  return 'validação ok';
+};
+
+const att = async ({ id }, { name }) => {
+  await productsModels.att(id, name);
+  return {
+    id,
+    name,
+  };
+};
+
 module.exports = {
   getAll,
   getById,
   add,
+  att,
+  validateProductId,
+  validateName,
 };
