bd790cc363358be1f8fda185517713526165039c
Leonidas <leonidas.drawanz@gmail.com>
2022-07-23 15:17:18 -0300

    fix: removendo cors

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const cors = require('cors');
 const swaggerFile = require('./swagger-output.json');
 const router = require('./routes/index');
 
@@ -12,7 +11,6 @@ app.get('/', (_request, response) => {
 
 app.use(express.json());
 app.use('/', router);
-app.use(cors());
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 module.exports = app;
